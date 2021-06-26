0f6f5057376b068e2b85111ca31c2c2d862ff971
Timur <36734601+TSecretT@users.noreply.github.com>
2021-06-27 01:01:35 +0600

    Update lemon.ts

--- src/contracts/lemon.ts
+++ src/contracts/lemon.ts
@@ -19,6 +19,7 @@ export class Lemon {
 		this.address = address;
 		this.contract = new this.web3.eth.Contract(this.LNFTABI, address);
 		this.defaultGasPrice = 20000000000;
+
 	}
 
 	fromBN(amount: any){
@@ -33,11 +34,25 @@ export class Lemon {
 		return await this.web3.eth.getGasPrice() || this.defaultGasPrice;
 	}
 
+	async totalSupply() {
+		return  await this.contract.methods.totalSupply().call();
+	}
+
+	async price(tokenID: number) {
+		let price =  await this.contract.methods.price(tokenID).call();
+		//return this.fromBN(new BigNumber(price).div(18));
+		return price;
+	}
+
+	async listed_map(tokenID: number) {
+		let listedMap =  await this.contract.methods.listedMap(tokenID).call();
+		//return this.fromBN(new BigNumber(price).div(18));
+		return listedMap;
+	}
+
 	async mint(tokenURI: string, sender: string, price: number , callback: any) {
-		const prec: any = new BigNumber(10).pow(new BigNumber(18));
-		let weiAmount: any = new BigNumber(price).times(prec);
+		let weiAmount: any = new BigNumber(price).times(18);
 		var gasPrice = await this.gasPrice();
-		console.log(weiAmount);
 		var tx = this.contract.methods.mint(tokenURI,sender,toBN(weiAmount));
 		let gasLimit = 150000;
 		try {
@@ -52,6 +67,59 @@ export class Lemon {
 		}, callback);
 	}
 
+
+	async buy(tokenID:number, sender: string, callback: any) {
+		// let weiAmount: any = new BigNumber(price).times(18);
+		let price: any = await this.price(tokenID);
+		var gasPrice = await this.gasPrice();
+		var tx = this.contract.methods.buy(tokenID);
+		let gasLimit = 150000;
+		try {
+			gasLimit = await tx.estimateGas({ value: 0, from: sender, to: this.address });
+		} catch(err) {
+			console.log("Gas limit error", err)
+		}
+		return tx.send({
+			from: sender,
+			value: price,
+			gasPrice: gasPrice,
+			gas: Math.round(gasLimit * 1.1)
+		}, callback);
+	}
+
+	async update_price(tokenID:number, sender: string, new_price:any, callback: any) {
+		const prec: any = new BigNumber(10).pow(new BigNumber(18));
+		let weiAmount: any = new BigNumber(new_price).times(prec);
+		var gasPrice = await this.gasPrice();
+		var tx = this.contract.methods.updatePrice(tokenID, toBN(weiAmount));
+		let gasLimit = 150000;
+		try {
+			gasLimit = await tx.estimateGas({ value: 0, from: sender, to: this.address });
+		} catch(err) {
+			console.log("Gas limit error", err)
+		}
+		return tx.send({
+			from: sender,
+			gasPrice: gasPrice,
+			gas: Math.round(gasLimit * 1.1)
+		}, callback);
+	}
+
+	async update_listed_status(tokenID:number, sender: string, status:string, callback: any) { //true or false
+		var gasPrice = await this.gasPrice();
+		var tx = this.contract.methods.updatePrice(tokenID, status);
+		let gasLimit = 150000;
+		try {
+			gasLimit = await tx.estimateGas({ value: 0, from: sender, to: this.address });
+		} catch(err) {
+			console.log("Gas limit error", err)
+		}
+		return tx.send({
+			from: sender,
+			gasPrice: gasPrice,
+			gas: Math.round(gasLimit * 1.1)
+		}, callback);
+	}
 
 
 
